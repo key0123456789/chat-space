19d026d4f432e9aa1f4c09e67ee0ba8988256686
key0123456789 <k.i-0123456789@live.jp>
2019-07-25 00:02:21 +0900

    change root_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
- root to: 'messages#index'
+ root 'groups#index'
  resources :users, only: [:edit, :update]
  resources :groups, only: [:new, :edit, :create, :update] 
   # resources :messages, only: [:index, :create]
