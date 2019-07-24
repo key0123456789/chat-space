c82414e34fac5b27cfd56ccf9a7ff0b07cd3cbfd
key0123456789 <k.i-0123456789@live.jp>
2019-07-24 18:39:19 +0900

    views groups

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
  root to: 'messages#index'
  resources :users, only: [:edit, :update]
+ resources :groups, only: [:new, :edit, :create, :update] 
+  # resources :messages, only: [:index, :create]
+ 
 end
