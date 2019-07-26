8516c708230f18abad50721b99e3f3801627f6cd
key0123456789 <k.i-0123456789@live.jp>
2019-07-26 12:04:47 +0900

    current_group members list

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,4 +4,21 @@ class Group < ApplicationRecord
   has_many :messages
 
   validates :name, presence: true, uniqueness: true
+
+  //
+  def show_members
+    group_member = []
+    users.each do |user|
+      group_member << user[:name]
+    end
+    return group_member
+  end
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      last_message.content? ? last_message.content : '画像が投稿されています'
+    else
+      'まだメッセージはありません。'
+    end
+  end
 end
