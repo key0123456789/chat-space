ced9fbbbbf6700ee1f509f4b6c8fb6113e9a708a
key0123456789 <k.i-0123456789@live.jp>
2019-08-05 20:08:52 +0900

    add async_function

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,46 @@
+$(function(){
+  function buildHTML(message){
+    var html = `
+      <div class="message">
+        <div class="message__upper-info">
+          <div class="message__upper-info__talker">
+            ${message.name}
+          </div>
+          <div class="message__upper-info__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <p class="message__text">
+            ${message.content}
+            
+            <img src='${message.image}' class="message-image">
+        </p>
+      </div>
+    `
+    return html;
+  }
+
+  $('#new_message').on('submit',function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html)
+      $('#message-content').val('')
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast' );
+    })
+    .fail(function(){
+      alert('error');
+    })
+    return false;
+  });
+});
