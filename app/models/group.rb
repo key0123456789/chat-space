2f4e1330e19a908faf4036557cbfc0cb8e501fe0
key0123456789 <k.i-0123456789@live.jp>
2019-08-11 11:14:16 +0900

    add or remove from groups

--- app/models/group.rb
+++ app/models/group.rb
@@ -5,15 +5,6 @@ class Group < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
 
-  
-  def show_members
-    group_member = []
-    users.each do |user|
-      group_member << user[:name]
-    end
-    return group_member
-  end
-
   def show_last_message
     if (last_message = messages.last).present?
       last_message.content? ? last_message.content : '画像が投稿されています'
