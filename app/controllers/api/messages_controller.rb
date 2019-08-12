ee0e2199bd938ef607239568e8073df25b178797
key0123456789 <k.i-0123456789@live.jp>
2019-08-13 01:48:47 +0900

    update automaticaly
    ・IDの差分で最新メッセージの有無を判定し、表示されていないメッセージがある場合のみ画面に表示。
    ・ビューの調整が必要。

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -2,11 +2,11 @@ class Api::MessagesController < ApplicationController
   def index
     group = Group.find(params[:group_id])
     last_message_id = params[:id].to_i
-    @messages = group.messages.includes(:user).where("id > #{last_message_id}")
+    @messages = group.messages.includes(:user).where('id >?',"#{last_message_id}")
 
     respond_to do |format|
       format.html
-      format.json
+      format.json {@messages}
     end
   end
 end
