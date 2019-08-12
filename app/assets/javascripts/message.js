a872746e069c59d6454b3fed9db9c3f5a23820db
key0123456789 <k.i-0123456789@live.jp>
2019-08-12 18:48:28 +0900

    get message.id

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,6 @@
 $(function(){
     function buildHTML(message){
-      var html = `<div class="message" data-id=${ message.id }>
+      var html = `<div class="message" data-id='${ message.id }'>
                     <div class="message__upper-info">
                       <div class="message__upper-info__talker">
                         ${ message.name }
@@ -14,14 +14,15 @@ $(function(){
                       <img src='${message.image}' class="message-image">
                     </p>
                   </div>`
-        
-      return html;
-    }
+
+                  return html;
+                }
 
     $('#new_message').on('submit',function(e){
       e.preventDefault();
       var formData = new FormData(this);
-      var url = $(this).attr('action')
+      var url = $(this).attr('action');
+
       $.ajax({
         url: url,
         type: "POST",
@@ -68,25 +69,29 @@ $(function(){
 
     var reloadMessages = function() {
       if($('.messages')[0]){
-        var last_message_id = $('.messages:last').data('id');
+        var last_message_id = $('div>.message:last').data('id');
+        // console.log(last_message_id);
       $.ajax ({
         url: "api/messages",
         type: 'GET',
         dataType: 'json',
         data: {id: last_message_id}
       })
-      .done(function(data) {
-      // console.log('sucess')
-        var insertHTML = buildHTML(data);
-      //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
-        messages.forEach(function(data){
-          var html = insertHTML(message);
-          $('.messages').append(html)
+      .done(function(messages) {
+      // messagesには投稿の全部のデータが配列で入っている。
+        var insertHTML = [];
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        if(last_message_id > $('.message:last').data('id')){
+          // console.log(messages);
+        messages.forEach(function(message){
+          insertHTML = buildHTML(message);
         })
+        $('.messages').append(insertHTML)
         $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast' );
-      })
+      }
+    })
       .fail(function() {
-        console.log('error');
+        alert('自動更新できませんでした');
       })
     }
   }
