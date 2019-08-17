b89267f2829f6ed0178245604263ea099e5e8d6d
key0123456789 <k.i-0123456789@live.jp>
2019-08-17 20:36:53 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,6 +12,10 @@ CarrierWave.configure do |config|
     region: 'ap-northeast-1'
   }
 
+  def size_range
+    1..2.megabytes
+  end
+
   config.fog_directory  = 'key0123456789'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/key0123456789'
 end
